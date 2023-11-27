c2a5381d09efe8d25a71fe08fb1f5465219be007
Hitcliff <memurya@mail.ru>
2023-11-27 16:21:41 +0300

    Последний шаг: устранить ненавязчивую логтческую ошибушку с итерациями и неправильными диапозонами

--- game.py
+++ game.py
@@ -1,23 +1,29 @@
 import random
+
+#TODO все исправно, но надо разобраться с итерациями и парами подсказок (ЛОГИКА БЛЯЯЯЯЯЯ)
 import customtkinter
 from CTkMessagebox import CTkMessagebox
 
 customtkinter.set_appearance_mode("Dark")
-customtkinter.set_default_color_theme("dark-blue")
 
-
-numbers_list = [i for i in range(1, 21)]
-win = random.choice(numbers_list)
 btn_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
             "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 lives = 0
 
-if win == 1:
-    lose = random.choice(numbers_list[1:])
-elif win == 20:
-    lose = random.choice(numbers_list[:19])
-else:
-    lose = random.choice(numbers_list[:win]+numbers_list[win+1:])
+
+def logic():
+    numbers_list = [i for i in range(1, 21)]
+
+    win = random.choice(numbers_list)
+
+    if win == 1:
+        lose = random.choice(numbers_list[1:])
+    elif win == 20:
+        lose = random.choice(numbers_list[:19])
+    else:
+        lose = random.choice(numbers_list[:win-1]+numbers_list[win:])
+
+    return win, lose
 
 
 # подсказки
@@ -37,10 +43,15 @@ def numbers_range(r):
 
 
 def main(scr, data):
-    global lives, win, lose
+    global lives, win, lose, btns
+
+    btn = btns[data]
 
     g, m = numbers_range(lives)
+
     if data == win:
+        btn.configure(fg_color="green")
+        scr.text.configure(text=f"Победа :D")
         msg = CTkMessagebox(title="Радостное окно)", message="Вы выиграли, хотите сыграть еще раз?",
                             icon="question", option_1="Да", option_2="Нет")
         response = msg.get()
@@ -48,9 +59,12 @@ def main(scr, data):
             scr.destroy()
         else:
             lives = 0
+            win, lose = logic()
             cmd()
 
     elif data == lose:
+        btn.configure(fg_color="red")
+        scr.text.configure(text=f"Ты проиграл(")
         msg = CTkMessagebox(title="Грустное окно(", message="Вы проиграли, хотите сыграть еще раз?",
                             icon="question", option_1="Да", option_2="Нет")
         response = msg.get()
@@ -58,13 +72,16 @@ def main(scr, data):
             scr.destroy()
         else:
             lives = 0
+            win, lose = logic()
             cmd()
 
     else:
-        print(f"Ноуноуноу мистер фиш. Попытай удачи в между {g} и {m}")
+        btn.configure(state="disabled")
+        scr.text.configure(text=f"Не угадал) попробуй найти между {g} и {m}")
 
     lives += 1
     if lives == 5:
+        scr.text.configure(text="Ты проиграл")
         msg = CTkMessagebox(title="Ни рыба, ни мясо окно :/", message="Вы конечно не выиграли, но и не проиграли."
                                                                       " Хотите сыграть еще раз?",
                             icon="question", option_1="Да", option_2="Нет")
@@ -73,6 +90,7 @@ def main(scr, data):
             scr.destroy()
         else:
             lives = 0
+            win, lose = logic()
             cmd()
 
 
@@ -83,20 +101,27 @@ class Window(customtkinter.CTk):
         self.btn_list = btn
 
     def mod_2(self):
+        global btns, lives
         self.title("Угадай число")
         self.geometry("800x600")
+        g, m = numbers_range(lives)
+        self.text = customtkinter.CTkLabel(master=self, text=f"1 подсказка: ищи ответ между {g} и {m}",
+                                      font=("Comic Sans MS", 30))
+        self.text.pack(expand=True)
+        lives += 1
         self.frame = customtkinter.CTkFrame(self, fg_color="#303030")
         self.frame.pack(side="bottom", padx=70, pady=(50, 80))
         self.destroy_list.append(self.frame)
+        self.destroy_list.append(self.text)
         row = 0
         column = 0
+        btns = {}
         for i in self.btn_list:
-            customtkinter.CTkButton(master=self.frame, text=i, command=lambda x=i: main(self, int(x)),
+            btn_i = (customtkinter.CTkButton(master=self.frame, text=i, command=lambda x=i: main(self, int(x)),
                                     font=("Comic Sans MS", 11), width=20,
-                                    fg_color="#484848", hover_color="#585858").grid(row=row,
-                                                                                    column=column, pady=3,
-                                                                                    padx=5, ipady=5, ipadx=53
-                                                                                    if int(i) < 10 else 50)
+                                    fg_color="#484848", hover_color="#585858"))
+            btns[int(i)] = btn_i
+            btn_i.grid(row=row,column=column, pady=3, padx=5, ipady=5, ipadx=53 if int(i) < 10 else 50)
             column += 1
             if column > 4 or column > 9 or column > 14:
                 column = 0
@@ -105,11 +130,11 @@ class Window(customtkinter.CTk):
     def mod_1(self):
         self.title("Угадай число")
         self.geometry("800x600")
-
         text_1 = customtkinter.CTkLabel(master=self, text="Угадай число", font=("Comic Sans MS", 38))
         text_1.pack(expand=True)
 
         btn_1 = customtkinter.CTkButton(master=self, text="Играть", font=("Comic Sans MS", 38), command=cmd)
+
         btn_1.pack(expand=True)
         self.destroy_list = [text_1, btn_1]
 
@@ -123,6 +148,7 @@ def cmd():
 
 
 game = Window(btn_list)
+win, lose = logic()
 game.mod_1()
 game.mainloop()
 
